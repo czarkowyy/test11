ef8550036c7f22f4524d7117f9616c206190abb3
czarkowyy <czarekstawicki12@gmail.com>
2024-12-19 14:10:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ document.querySelector(".go").addEventListener('click', () => {
     var firstname = document.querySelector(".firstname");
     var surname = document.querySelector(".surname");
     var image = document.querySelector(".image");
+    var pesel = document.querySelector(".PESEL");
 
     var params = new URLSearchParams();
     params.set("firstname", firstname.value);
     params.set("surname", surname.value);
     params.set("image", image.value);
+    params.set("PESEL",pesel.value);
     location.href = "id.html?" + params;
 });
