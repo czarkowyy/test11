60fde82e930dc43a2c8664aa9d31c71ce89cb393
czarkowyy <czarekstawicki12@gmail.com>
2024-12-19 14:16:37 +0100

    Update id.js

--- id.js
+++ id.js
@@ -4,6 +4,7 @@ var params = new URLSearchParams(window.location.search);
 var firstname = params.get("firstname");
 var surname = params.get("surname");
 var image = params.get("image");
+var pesel = params.get("pesel");
 
 function hideAddressBar() {
 if (document.documentElement.scrollHeight < window.outerHeight / window.devicePixelRatio)
